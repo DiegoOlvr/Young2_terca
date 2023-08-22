9aa9cebb124de79980ae626797769246fea5ba4c
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2023-08-22 20:21:53 -0300

    Aula 14 - Intro JS

---
+++ Aula 14/teste.js
@@ -0,0 +1,103 @@
+// COMENTÁRIO
+
+/**
+ * COMENTÁRIO
+ * DE
+ * MULTIPLAS
+ * LINHAS 
+ */
+
+// TIPOS DE DADOS
+/**
+ * NUMBER => int float NaN 
+ * STRING => texto
+ * BOOLEAN => true false
+ * NULL => vazio
+ * UNDEFINED => não tem definição 
+ */
+
+/**
+ * # CONVERTER TIPOS
+ * De número para string
+ * var.toString()
+ * 
+ * De string para número
+ * parseInt(var)
+ */
+
+/**
+ * # OPERADORES ARITMÉTICOS
+ * + ADIÇÃO
+ * - SUBTRAÇÃO
+ * * MULTIPLICAÇÃO
+ * / DIVISÃO
+ * % RESTO DA DIVISÃO
+ * ** POTENCIA
+ */
+
+/**
+ * # OPERADORES RELACIONAIS
+ * > MAIOR QUE
+ * < MENOR QUE
+ * >= MAIOR OU IGUAL QUE
+ * <= MENOR OU IGUAL QUE
+ * == IGUALDADE
+ * === SE OS TIPOS SÃO IGUAIS
+ * != DIFERENTE
+ */
+
+/**
+ * # OPERADORES LÓGICOS
+ * '!  NÃO - NEGAÇÃO
+ * '&& E - CONJUNÇÃO
+ * '|| OU - DISJUNÇÃO
+ */
+
+
+/**
+ * # VARIÁVEIS
+ * VAR   = vc pode redefinir - escopo global
+ * LET   = vc não pode redefinir - escopo local
+ * CONST = constante, uma informação que vc não irá mudar
+ */
+
+/**
+ * # ARRAY   = LISTAS
+ * # OBJETOS = DICIONÁRIO
+ * # JSON => JAVASCRIPT OBJECT NOTATION {chave: valor}
+ * 
+ * Para saber o tipo de algum dado
+ * 
+ * ! typeof()
+ */
+
+/**
+ * # FUNÇÕES
+ * function nome_da_função(parametros){
+ *     comandos
+ *     return alguma_coisa
+ * }
+ */
+
+/**
+ * # CONDICIONAIS
+ * IF (CONDIÇÃO1){
+ *    PLANO A
+ * }ELSE IF (CONDIÇÃO2){
+ *    PLABO B
+ * }ELSE {
+ *    PLANO RESTO
+ * }
+ */
+
+/**
+ * # LOOP
+ * for (let i = 0; i < 10; i++){
+ *     o que tem que ser repetido
+ * }
+ */
+
+
+document.getElementById('titulo').innerHTML = 'Bom dia'
+document.write(5 + 5)
+console.log('Teste 123')
