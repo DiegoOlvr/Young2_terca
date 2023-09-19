93d4e1b182887a7c6acfbfe4bbcbdb99e3c98b4b
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2023-09-19 20:04:56 -0300

    Aula 15 - Hora atual

--- Aula 15/script.js
+++ Aula 15/script.js
@@ -1,3 +1,24 @@
 var msg = document.getElementById('msg')
 var img = document.getElementById('foto')
-let numero = window.prompt('Digite um número')
+var horario = document.getElementById('horario')
+let hora = window.prompt('Digite um número')
+var data = new Date();
+var hora2 = data.getHours();
+console.log(horario)
+
+horario.innerText = `Agora são ${hora} horas`
+
+if (hora >= 5 && hora < 12) {
+    document.body.style.background = 'orange';
+    msg.innerHTML = 'Bom dia!';
+    img.src = ''
+}
+else if (hora > 12 && hora <= 18) {
+    document.body.style.background = 'red';
+    msg.innerHTML = 'Boa tarde!';
+}
+else {
+    document.body.style.background = 'darkblue';
+    msg.innerHTML = 'Boa noite!';
+    img.src = './img/boanoite.jpg'
+}
