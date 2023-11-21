379aff4e0d2c8dcc8e010f80c6c6db01e362d67f
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2023-11-21 19:42:26 -0300

    Finalizando pokedex

--- Aula_23/script.js
+++ Aula_23/script.js
@@ -6,6 +6,8 @@ const input = document.querySelector('.input-search')
 const btn_anterior = document.querySelector('.btn-anterior')
 const btn_proximo = document.querySelector('.btn-proximo')
 
+let id_pokemon = 1
+
 
 const fetch_pokemon = async (pokemon) => {
     api_response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -17,13 +19,42 @@ const fetch_pokemon = async (pokemon) => {
 }
 // fetch_pokemon('pikachu')
 const render_pokemon = async (pokemon) => {
+    poke_name.innerHTML = 'Pesquisando...'
+    poke_number.innerHTML = ''
+
     const info = await fetch_pokemon(pokemon)
     if (info) {
         poke_img.src = info['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
         poke_img.style.display = 'block'
         poke_name.innerHTML = info.name
         poke_number.innerHTML = info.id
+        id_pokemon = info.id
+        input.value = ''
+    }
+    else {
+        poke_img.style.display = 'none'
+        poke_name.innerHTML = 'Não encontrado'
+        poke_number.innerHTML = ''
+        input.value = ''
     }
 }
 
-render_pokemon('ditto')
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    let pokemon = input.value.toLowerCase()
+    render_pokemon(pokemon)
+
+})
+
+btn_anterior.addEventListener('click', () => {
+    if (id_pokemon > 1) {
+        id_pokemon -= 1
+        render_pokemon(id_pokemon)
+    }
+})
+btn_proximo.addEventListener('click', () => {
+        id_pokemon += 1
+        render_pokemon(id_pokemon)
+})
+
+render_pokemon(id_pokemon)
